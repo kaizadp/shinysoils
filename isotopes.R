4dfacec0b4f8aa5731ad415e77ecd57f92d36fae
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-08-15 19:57:09 -0700

    change theme. add calculations. clean up.

--- isotopes.R
+++ isotopes.R
@@ -3,58 +3,64 @@
 library(shiny)
 
 ui = (fluidPage(
+  theme = shinytheme("united"),
+  
   titlePanel("Stable Isotopes"),
-  h5("Use these tools to for basic calculations for stable isotopes"),
+  h5("Use these tools for basic calculations for stable isotopes"),
   br(),
-  # 1. R to delta --------------------------------------------------------------
   navlistPanel(
-    "R to delta",
-    tabPanel("C",
+    # 1. convert R to delta --------------------------------------------------------------
+    "convert R to δ",
+    tabPanel("13C-carbon",
              h3("input enrichment"),
              textInput("enrich_c", " ", value = "0.10"),
              br(),
              h3("δ-13C"),
              textOutput("delta_c")),
-    tabPanel("N",
+    tabPanel("15N-nitrogen",
              h3("input enrichment"),
              textInput("enrich_n", " ", value = "0.10"),
              br(),
              h3("δ-15N"),
              textOutput("delta_n")),
-  
-  # 2. target enrichment ----------------------------------------------------------------------
-    "target enrichment",
-    tabPanel("calc",
-             h5("Use this tool to calculate the enrichment of a mixture"),
+    tabPanel("calculations",
+             "atom percent 13C = [13C/(12C + 13C)]100",
+             br(),
+             br(),
+             "fractional abundance of 13C ≡ 13F 13F = 13C/(12C + 13C)",
+             br(),
+             br(),
+             "carbon isotope ratio” = 13C/12C ≡ 13R",
+             br(),
+             br(),
+             "13F = 13R/(1 + 13R)",
+             br(),
+             br(),
+             "δ = (R-sample/R-standard - 1) * 1000"),
+    
+    # 2. target enrichment ----------------------------------------------------------------------
+    "labelled mixtures",
+    tabPanel("enrichment of a mixture",
+             h5("Enter enrichment/mass for each compound to calculate the enrichment of the mixture"),
              sidebarLayout(position = "left",
                            sidebarPanel(
                              h3("compound 1"),
-                             textInput("R1", "enrichment", value = "0.010"),
-                             textInput("M1", "mass", value = "100"),
+                             textInput("R1", "enrichment (R1)", value = "0.010"),
+                             textInput("M1", "mass (M1)", value = "100"),
                              
                              h3("compound 2"),
-                             textInput("R2", "enrichment", value = "0.99"),
-                             textInput("M2", "mass", value = "1")
-                           ),
-                           
-                           position = "right",
-                           sidebarPanel(
-                             h3("compound 1"),
-                             textInput("R1", "enrichment", value = "0.010"),
-                             textInput("M1", "mass", value = "100"),
-                             
-                             h3("compound 2"),
-                             textInput("R2", "enrichment", value = "0.99"),
-                             textInput("M2", "mass", value = "1")
+                             textInput("R2", "enrichment (R2)", value = "0.99"),
+                             textInput("M2", "mass (M2)", value = "1")
                            ),
                            
                            mainPanel(
-                             h2("mass"),
+                             h2("mass (M)"),
                              textOutput("mass_total"),
-                             h2("enrichment"),
+                             h2("enrichment (R)"),
                              textOutput("enrich_total")
                            )
-             )))))
+             ))
+  )))
                            
   
   # server ----
